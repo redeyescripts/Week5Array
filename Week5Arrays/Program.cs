eaf506b24ef414cb515064668b8aaeda14b7ee01
Opilane TTHK <opilane@tthk.ee>
2020-10-01 12:12:09 +0300

    first commit

---
+++ Week5Arrays/Program.cs
@@ -0,0 +1,66 @@
+﻿
+
+
+using System;
+using System.Xml.Schema;
+
+namespace Week5Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            // int[] numbers - massiiv
+            int[] array1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            foreach (int number in array1)
+            {
+                Console.WriteLine(number);
+            }
+            //option 2
+            int[] array2 = new int[10];
+            array2[0] = 1;
+            array2[1] = 2;
+            array2[2] = 3;
+            array2[3] = 4;
+            array2[4] = 5;
+            array2[5] = 6;
+            array2[6] = 7;
+            array2[7] = 8;
+            array2[8] = 9;
+            array2[9] = 10;
+            foreach (int number in array2)
+            {
+                Console.WriteLine($"array 2: {number}");
+            }
+            //Option 3 auto populat array with numbers
+
+            int[] array3 = new int[10];
+            for (int i = 0; 1 < array3.Length; i++)
+            {
+                array3[i] = i+1;
+
+            }
+            foreach (int number in array3)
+            {
+                Console.WriteLine($"Array3:{number}");
+            }
+            int[] array4 = new int[10];
+            array2[9] = 10;
+            array2[8] = 9;
+            array2[7] = 8;
+            array2[6] = 7;
+            array2[5] = 6;
+            array2[4] = 5;
+            array2[3] = 4;
+            array2[2] = 3;
+            array2[1] = 2;
+            array2[0] = 1;
+            foreach (int number in array4)
+            {
+                Console.WriteLine($"array 4: {number}");
+            }
+        }
+    }
+}
---
+++ Week5Arrays2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Week5Arrays2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+           // int[] array1 = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
+            //foreach (int number in array1)
+            {
+               // Console.WriteLine(number);
+            }
+            int[] array4 = new int[10];
+
+            for(int i = array4.Length - 1,j =0; i >= 0; i--,j++)
+            {
+                array4[j] = i +1;
+
+            }
+            foreach(int num in array4 )
+            {
+                Console.WriteLine(num);
+            }
+            Random rnd = new Random();
+        int[] array5 = new int[10];
+
+        for(int i =0; i < array5.Length; i++)
+            {
+
+                array5[i] = rnd.Next(1, 101);
+
+            }
+              foreach(int num in array5)
+              {
+                Console.WriteLine(num);
+              }
+        }  
+        
+          
+            
+            
+    }
+}
